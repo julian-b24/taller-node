161c422b3efdac087d718aca85737cbb90c9fcfa
julian-b24 <julian.camilob24@gmail.com>
2023-11-11 23:17:43 -0500

    Fix in group model

--- src/models/group.model.ts
+++ src/models/group.model.ts
@@ -12,9 +12,8 @@ export  interface GroupDocument extends GroupInput, mongoose.Document {
 }
 
 const groupSchema = new mongoose.Schema({
-        name: {type: String, required: true},
-        email: {type: String, required: true, index: true, unique: true},
-        password: {type: String, required: true}
+        name: {type: String, required: true, index: true},
+        users: {type: String, required: true}
     }, {timestamps: true, collection: "groups"});
 
 const Group = mongoose.model<GroupDocument>("Group", groupSchema);
